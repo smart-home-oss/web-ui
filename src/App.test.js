387ede254e826ca64c4ec15ddae56f38c51cfcd8
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-01-28 19:51:14 +0100

    make the test green, no logic though

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
+// @flow
+
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = getByText(/Houses/i);
   expect(linkElement).toBeInTheDocument();
 });
